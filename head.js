f997ef384c1e9d867534a7a13c8eac1356e7f16f
Sven Bellamy <sven.bellamy@gmail.com>
2021-03-30 22:23:03 +0000

    changes to head.js

---
+++ head.js
@@ -0,0 +1,19 @@
+// create a function 'head' which returns the first item in the array
+function head(arr) {
+  for (let i in arr) return arr[i];
+}
+console.log(head(['a', 'b', 'c']));
+
+// use assertEqual to write test cases for various scenarios
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+// TEST CODE
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
