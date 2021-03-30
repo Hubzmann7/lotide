952d75088e62bde6f2a89c6fba5bef73d8b5d78a
Sven Bellamy <sven.bellamy@gmail.com>
2021-03-30 21:01:44 +0000

    assertion function with emoji missing

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('blue', 'blue');
+assertEqual('red', 'blue');
+assertEqual(1, 5);
+
+console.log(assertEqual);
